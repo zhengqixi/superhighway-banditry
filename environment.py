af3ebb5efddfd090d939870a0010cf8ee8eb3325
Zhengqi Xi <zhengqixi@users.noreply.github.com>
2020-12-09 17:53:10 -0500

    Add implementation of BaSE Algorithm. Modify environment to support batch arm pulls (#3)
    * git magic
    
    * Add BaSE implementation attempt
    
    * Allow create from dict
    
    * replicate paper results
    
    * Add batched penalty logic

--- environment.py
+++ environment.py
@@ -1,6 +1,8 @@
-from typing import List, Tuple, Type, TypeVar
+from typing import Dict, List, Tuple, Type, TypeVar
 from itertools import groupby
 from numpy.random import default_rng
+import numpy as np
+import math
 import json
 
 Highway = TypeVar('Highway', bound='Highway')
@@ -10,108 +12,162 @@ class Highway():
 
     class Arm:
         def __init__(self, id: str, mean: float, variance: float) -> None:
-            self.__id = id
-            self.__mean = mean
-            self.__variance = variance
+            self._id = id
+            self._mean = mean
+            self._variance = variance
 
         @property
         def id(self) -> str:
-            return self.__id
+            return self._id
 
         @property
         def mean(self) -> float:
-            return self.__mean
+            return self._mean
 
         @property
         def variance(self) -> float:
-            return self.__variance
+            return self._variance
 
     class Node:
         def __init__(self, id: str, failure: float) -> None:
-            self.__id = id
-            self.__failure = failure
+            self._id = id
+            self._failure = failure
 
         @property
         def id(self) -> str:
-            return self.__id
+            return self._id
 
         @property
         def failure_probability(self) -> float:
-            return self.__failure
+            return self._failure
 
     def __init__(self, arms: List[Tuple[str, float, float]], nodes: List[Tuple[str, float]], mapping: List[Tuple[str, str]], failure_penalty: float = 0, seed: int = None) -> None:
-        self.__failure_penalty = failure_penalty
+        self._failure_penalty = failure_penalty
 
-        self.__arms = [self.Arm(x[0], x[1], x[2]) for x in arms]
-        self.__arms_dict = {x.id: x for x in self.__arms}
-        if len(self.__arms_dict) != len(self.__arms):
+        self._arms = [self.Arm(x[0], x[1], x[2]) for x in arms]
+        self._arms_dict = {x.id: x for x in self._arms}
+        if len(self._arms_dict) != len(self._arms):
             raise Exception("Duplicate arm ids detected")
 
         if len(list(filter(lambda x: x[1] >= 0 and x[1] <= 1, nodes))) != len(nodes):
             raise Exception("Nodes with invalid failure probability detected")
-        self.__nodes = [self.Node(x[0], x[1]) for x in nodes]
-        self.__nodes_dict = {x.id: x for x in self.__nodes}
-        if len(self.__nodes) != len(self.__nodes_dict):
+        self._nodes = [self.Node(x[0], x[1]) for x in nodes]
+        self._nodes_dict = {x.id: x for x in self._nodes}
+        if len(self._nodes) != len(self._nodes_dict):
             raise Exception("Duplicate node ids detected")
 
-        if len(mapping) != len(list(filter(lambda x: x[0] in self.__arms_dict, mapping))):
+        if len(mapping) != len(list(filter(lambda x: x[0] in self._arms_dict, mapping))):
             raise Exception("Mapping has arms not in arms list")
         grouped_mapping = groupby(mapping, lambda x: x[0])
-        self.__arms_to_nodes = {x[0]: [self.__nodes_dict[y[1]]
-                                       for y in x[1]] for x in grouped_mapping}
+        self._arms_to_nodes = {x[0]: [self._nodes_dict[y[1]]
+                                      for y in x[1]] for x in grouped_mapping}
         if seed is not None:
-            self.__generator = default_rng(seed)
+            self._generator = default_rng(seed)
         else:
-            self.__generator = default_rng()
+            self._generator = default_rng()
 
-    def step(self, arm: str) -> Tuple[float, bool]:
-        if arm not in self.__arms_dict:
+    def pull_arm(self, arm: str) -> Tuple[float, bool]:
+        if arm not in self._arms_dict:
             raise Exception("{} is not a valid arm to pull".format(arm))
-        arm_obj = self.__arms_dict[arm]
-        arm_reward = self.__generator.normal(
+        arm_obj = self._arms_dict[arm]
+        arm_reward = self._generator.normal(
             loc=arm_obj.mean, scale=arm_obj.variance)
         penalty_applied = False
-        if arm in self.__arms_to_nodes:
-            failure_threshold = self.__generator.uniform()
-            for node in self.__arms_to_nodes[arm]:
+        if arm in self._arms_to_nodes:
+            failure_threshold = self._generator.uniform()
+            for node in self._arms_to_nodes[arm]:
                 if node.failure_probability >= failure_threshold:
-                    arm_reward -= self.__failure_penalty
+                    arm_reward -= self._failure_penalty
                     penalty_applied = True
+                    break
         return [arm_reward, penalty_applied]
 
-    @property
+    def pull_arm_n_times(self, arm: str, n: int, omit_penalty: bool = False) -> Tuple[List[float], List[bool]]:
+        if arm not in self._arms_dict:
+            raise Exception("{} is not a valid arm to pull".format(arm))
+        arm_obj = self._arms_dict[arm]
+        arm_rewards = self._generator.normal(
+            loc=arm_obj.mean, scale=arm_obj.variance, size=n)
+        if arm not in self._arms_to_nodes:
+            penalty_bool = None
+            if not omit_penalty:
+                penalty_bool = np.full(n, fill_value=False)
+            return (arm_rewards, penalty_bool)
+        else:
+            failure_probabilities = self._generator.uniform(size=n)
+            apply_penalty = np.full(n, fill_value=False)
+            for node in self._arms_to_nodes[arm]:
+                apply_penalty_for_node = failure_probabilities > node.failure_probability
+                apply_penalty = np.logical_or(
+                    apply_penalty, apply_penalty_for_node)
+            arm_rewards = arm_rewards - self._failure_penalty * \
+                np.array(apply_penalty, dtype=int)
+            if omit_penalty:
+                return (arm_rewards, [])
+            else:
+                return (arm_rewards, apply_penalty)
+
+    @ property
     def arms(self):
-        return [x.id for x in self.__arms]
+        return [x.id for x in self._arms]
 
-    @property
+    @ property
     def nodes(self):
-        return [x.id for x in self.__nodes]
+        return [x.id for x in self._nodes]
 
-    @property
+    @ property
     def arms_to_nodes(self):
-        return {x[0]: [y.id for y in x[1]] for x in self.__arms_to_nodes.items()}
+        return {x[0]: [y.id for y in x[1]] for x in self._arms_to_nodes.items()}
 
-    @property
+    @ property
     def failure_penalty(self):
-        return self.__failure_penalty
+        return self._failure_penalty
 
-    @classmethod
+    @ property
+    def best_arm(self) -> Tuple[str, float]:
+        '''
+        The best arm will be the one that has the highest expected value.
+        In our case, the expected value will be:
+        probability that no node fails * arm's mean + probability that at least one node fails * (arm's mean - penalty)
+        '''
+        best_so_far = ("", -math.inf)
+        for arm in self._arms:
+            arm_expected_value = best_so_far[1]
+            if arm.id not in self._arms_to_nodes:
+                arm_expected_value = arm.mean
+            else:
+                linked_nodes = self._arms_to_nodes[arm.id]
+                no_failure_probability = math.prod([
+                    1 - x.failure_probability for x in linked_nodes])
+                at_least_one_failure_probability = 1 - no_failure_probability
+                arm_expected_value = no_failure_probability*arm.mean + \
+                    at_least_one_failure_probability * \
+                    (arm.mean - self._failure_penalty)
+            if arm_expected_value > best_so_far[1]:
+                best_so_far = (arm.id, arm_expected_value)
+        return best_so_far
+
+    @ classmethod
     def from_json_file(cls: Type[Highway], filename: str) -> Highway:
         with open(filename, 'r') as json_file:
             json_dump = json.load(json_file)
-            arms = json_dump["arms"]
-            nodes = json_dump["nodes"]
-            penalty = 0
-            if "failure_penalty" in json_dump:
-                penalty = json_dump["failure_penalty"]
-            mapping = []
-            if "mapping" in json_dump:
-                mapping = json_dump["mapping"]
-            highway = cls(arms=arms, nodes=nodes,
-                          mapping=mapping, failure_penalty=penalty)
-            seed = None
-            if "seed" in json_dump:
-                seed = json_dump["seed"]
-            highway = cls(arms=arms, nodes=nodes,
-                          mapping=mapping, failure_penalty=penalty, seed=seed)
-            return highway
+            return Highway.from_dict(json_dump)
+
+    @ classmethod
+    def from_dict(cls: Type[Highway], builder: Dict) -> Highway:
+        arms = builder["arms"]
+        nodes = builder["nodes"]
+        penalty = 0
+        if "failure_penalty" in builder:
+            penalty = builder["failure_penalty"]
+        mapping = []
+        if "mapping" in builder:
+            mapping = builder["mapping"]
+        highway = cls(arms=arms, nodes=nodes,
+                      mapping=mapping, failure_penalty=penalty)
+        seed = None
+        if "seed" in builder:
+            seed = builder["seed"]
+        highway = cls(arms=arms, nodes=nodes,
+                      mapping=mapping, failure_penalty=penalty, seed=seed)
+        return highway
